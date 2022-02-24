5be469f12c7837416702fb7355a79971adb81d19
AshveerB <ashveerbhayroo@gmail.com>
2022-02-23 20:07:40 -0800

    send template email

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+
+	const handleClick = (e) => {
+		e.preventDefault();
+
+		if (e.target.id === 'name') {
+			setName(e.target.value);
+		} else {
+			setEmail(e.target.value);
+		}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const dataToSubmit = {
+			name,
+			email,
+		};
+
+		axios.post('api/sendMail', dataToSubmit);
+	};
+
+	return (
 		<div className='App'>
 			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<input />
-				<input />
-        <button></button>
+				<form onSubmit={handleSubmit}>
+					<input
+						id='name'
+						placeholder='Name'
+						value={name}
+						onChange={handleClick}
+					/>
+					<input
+						id='email'
+						placeholder='Email'
+						value={email}
+						onChange={handleClick}
+					/>
+					<button onClick={handleSubmit}>Send Email</button>
+				</form>
 			</header>
 		</div>
 	);
